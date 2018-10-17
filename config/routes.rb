356bcb4cf83f7d3ceb6a3ddef85cc1551e1bf41d
ikeda_coffee <kouhik06@gmail.com>
2018-10-18 00:08:12 +0900

    ルーティングの設定とseedデータの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  unauthenticated :user do
+    devise_scope :user do
+      root to: 'devise/sessions#new'
+    end
+  end
+
+  authenticated :user do
+    root to: 'comments#index'
+  end
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(display_name: "山田太郎", email: "yamada@example.com", password: "password")
+User.create(display_name: "田中花子", email: "tanaka@example.com", password: "password")
+User.create(display_name: "鹿島田楓", email: "kashimada@example.com", password: "password")
