442d0bf17d639d4d6c57dddc2c77e9498e57287b
ikeda_coffee <kouhik06@gmail.com>
2018-10-18 07:22:16 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     root to: 'comments#index'
   end
 
-  resources :comments, only: [:index, :new, :create]
+  resources :comments, only: [:new, :create]
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
