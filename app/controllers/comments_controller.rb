9dba9fb1745104027fa66249b85255213bf5ccbd
ikeda_coffee <kouhik06@gmail.com>
2018-10-18 05:30:46 +0900

    ひとこと作成機能の追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,25 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
+    @users = User.all
+  end
+
+  def new
+    @comment = current_user.comments.build
+  end
+
+  def create
+    @comment = current_user.comments.build(comment_params)
+    if @comment.save
+      redirect_to root_url, notice: (I18n.t 'notice.create_comment')
+    else
+      render :new
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
     root to: 'comments#index'
   end
 
+  resources :comments, only: [:index, :new, :create]
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
